46d4176828be3db63c601b7655e4f8e3f1eced00
Biljana Ljubojevic <biljana.ljubojevic@etondigital.com>
2019-02-08 16:11:15 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+jQuery(($) => {
+  const jsLoaded = () => {
+    console.log('js is loaded');
+  }
+
+  /* ====== DOCUMENT READY ====== */
+  $(document).ready(() => {
+    jsLoaded ();
+  });
+
+   /* ====== resize ====== */
+  // $(window).resize(() => {
+ 
+  // });
+
+  /* ====== scroll ====== */
+  // $(window).scroll((e) => {
+  
+  // });
+});
